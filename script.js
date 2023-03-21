cc92c8ce31fb3f6055077d007bb958f50db9888c
QuentinPacheu <quentin.pacheu@gmail.com>
2023-03-21 23:58:39 +0100

    rendu

--- script.js
+++ script.js
@@ -1,14 +1,71 @@
-let mobilenet;
-let video;
+let faceapi;
+let detections = [];
 
-function setup() {
-    createCanvas(640, 480);
+let video;
+let canvas;
+
+function setup(){
+    canvas = createCanvas(480, 360);
+    canvas.id('canvas');
     video = createCapture(VIDEO);
-    video.hide();
-    background(0);
-    mobilenet = ml5.imageClassifier('MobliNet');
+    video.id('video');
+    video.size(width, height);
+
+    const faceOptions = {
+        withLandmarks: true,
+        withExpressions: true,
+        withDescriptions: false,
+        minConfiendence: 0.5
+    };
+
+    faceapi = ml5.faceApi(video, faceOptions, faceReady);
+}
+
+function faceReady(){
+    faceapi.detect(gotFaces);
+}
+
+function gotFaces(error, result){
+    if(error){
+        console.log(error);
+        return
+    }
+
+    detections = result;
+    console.log(detections);
+    faceapi.detect(gotFaces);
+}
+function draw(){
+    clear();
+    drawBoxes(detections);
+    drawLandmarks(detections);
+
     
 }
-function draw() {
-    image(video, 0, 0);
+function drawBoxes(detections){
+    if(detections.length > 0){
+        for(f = 0; f < detections.length; f++){
+            
+
+            let {_x, _y, _width, _height} = detections[0].alignedRect._box;
+
+            stroke(44, 169, 225);
+            strokeWeight(1);
+            noFill();
+            rect(_x, _y, _width, _height);
+        }
+    }
+}
+
+function drawLandmarks(detections){
+    if(detections.length > 0){
+        for(f = 0; f < detections.length; f++){
+            let points = detections[f].landmarks.positions;
+            for(let i = 0; i < points.length; i++){
+                stroke(44, 169, 225);
+                strokeWeight(3);
+                point(points[i]._x, points[i]._y);
+            }
+        }
+    }
 }
